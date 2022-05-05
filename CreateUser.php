c88711c018c579933f39953b76237bf6abf36d62
Chetan-Hiremath <97000301+Chetan-Hiremath@users.noreply.github.com>
2022-05-05 11:55:20 -0500

    Create CreateUser.php

---
+++ CreateUser.php
@@ -0,0 +1,18 @@
+<?php
+$conn = new mysqli("129.237.87.5", "c743h357", "pieJeo7g", "c743h357");
+if ($conn->connect_error)
+{
+  die("Connection failed: " . $conn->connect_error);
+}
+$name = $_POST['name'];
+$sql = "INSERT INTO Users (UserID) VALUES ('$name')";
+if ($conn->query($sql) == TRUE) 
+{
+  echo "New user is created successfully!";
+}
+else 
+{
+  echo "The user already exists!";
+}
+$conn->close();
+?>
