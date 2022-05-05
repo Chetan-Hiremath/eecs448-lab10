8beca814bbbefb62e873fe8a59b53c5aa3861646
Chetan-Hiremath <97000301+Chetan-Hiremath@users.noreply.github.com>
2022-05-05 12:12:39 -0500

    Create ViewUsers.php

---
+++ ViewUsers.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+<table border =\"5\" style='border-collapse: collapse'> 
+<b>Users: </b><br>
+<p></p>
+<tr>
+<th>User ID</th>
+</tr>
+<?php
+$conn = new mysqli("129.237.87.5", "c743h357", "pieJeo7g", "c743h357");
+if ($conn->connect_error)
+{
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT UserID FROM Users";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) 
+{
+   while($row = $result->fetch_assoc()) 
+   {
+      echo "<tr><td>" . $row["UserID"]. "</td></tr>";
+   }
+   echo "</table>";
+} 
+$conn->close();
+?>
+</table>
+</body>
+</html>
