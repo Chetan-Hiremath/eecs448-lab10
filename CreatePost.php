9ef2666510ac05e8cb68cb4538c50a0752501412
Chetan-Hiremath <97000301+Chetan-Hiremath@users.noreply.github.com>
2022-05-05 11:58:55 -0500

    Create CreatePost.php

---
+++ CreatePost.php
@@ -0,0 +1,45 @@
+<?php
+    $user = $_POST["name"];
+    $post = $_POST["post"];
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "c743h357", "pieJeo7g", "c743h357");
+    if($mysqli->connect_errno){
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+    $query = "SELECT UserID FROM Users";
+    $Table = false;
+    if($result = $mysqli->query($query))
+    {
+        while($row = $result->fetch_assoc())
+        {
+            if($row['UserID'] == $user)
+            {
+                $Table = true;
+            }
+        }
+    }
+    if($user == "")
+    {
+        echo "The username should not be empty!";
+    }
+    else if(!$Table)
+    {
+        echo "The user doesn't exist!";
+    }
+    else if($post == "")
+    {
+        echo "The post should not be empty!";
+    }
+    else
+    { 
+        $sql = "INSERT INTO Posts (Content, AuthorID) VALUES ('".$post."', '".$user."')";
+        if($mysqli->query($sql) === TRUE)
+        {
+            echo "New post is created successfully!";
+        }
+        else
+        {
+            echo "The user already exists!";
+        }     
+    }
+?>
